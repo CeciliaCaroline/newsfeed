19c1ad9793b916412c791e54c2603bbd56f0c07c
Adrian Sebuliba <adrian.sebuliba1@gmail.com>
2017-08-18 12:58:17 +0300

    Setting up CLI docstring

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+The  NewsFeeds Program.
+
+Usage:
+    primfeed view_feed
+    primfeed post <title> <body>
+    primfeed comment <postId> <title> <body>
+    primfeed (-i | --interactive)
+    primfeed (-h | --help | --version)
+
+Options:
+    -i, --interactive  Interactive Mode
+    -h, --help  Show this screen and exit.
+"""
